801efc6a3d1b35781375234e88d4c3ce59e0c8ab
Jai Kushwaha <kushwahajai74@gmail.com>
2023-07-06 13:32:39 +0530

    optimized backend2

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -5,7 +5,7 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     httpOnly: true,
     expires: new Date(
-      Date.now + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
   };
   res.status(statusCode).cookie("token", token, options).json({
